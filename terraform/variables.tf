0ac0c1af630c8a97902005393dbeadb3c3cbffc4
sh0e1 <sh0e1@users.noreply.github.com>
2020-02-08 22:21:13 +0900

    :sparkles: Add: terraform files

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+  type = string
+}
+
+variable "credentials_file" {
+  type = string
+}
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type = string
+  default = "us-central1-c"
+}
